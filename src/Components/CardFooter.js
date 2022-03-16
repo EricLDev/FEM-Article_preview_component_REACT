6d8639db0d2ee9b51e3c5563b1617206acf1dccb
Eric Lehmann <lehm_eric@yahoo.fr>
2022-03-16 14:04:27 +0100

    added JS to footer ShareBTN

--- src/Components/CardFooter.js
+++ src/Components/CardFooter.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./CardFooter.module.css";
 
 const CardFooter = () => {
+	const [buttonIsClicked, setButtonIsClicked] = useState(false);
+
 	return (
-		<div className={classes.cardFooter}>
-			<img className={classes.avatar} src="images/avatar-michelle.jpg" alt="" />
-			<div className={classes.nameDate}>
-				<p>Michelle Appleton</p>
-				<p>28 Jun 2020</p>
-			</div>
-			<button className={classes.share}>
-				<img src="images/icon-share.svg" alt="" />
-			</button>
+		<div>
+			{buttonIsClicked ? (
+				<div className={`${classes.cardFooter} ${classes.footerClicked}`}>
+					<p>SHARE</p>
+					<div className={classes.socialIcons}>
+						<img src="images/icon-facebook.svg" alt="" />
+						<img src="images/icon-twitter.svg" alt="" />
+						<img src="images/icon-pinterest.svg" alt="" />
+					</div>
+					<button className={classes.shareBtn} onClick={() => setButtonIsClicked(!buttonIsClicked)}>
+						<img className={classes.shareClicked} src="images/icon-share.svg" alt="" />
+					</button>
+				</div>
+			) : (
+				<div className={classes.cardFooter}>
+					<img className={classes.avatar} src="images/avatar-michelle.jpg" alt="" />
+					<div className={classes.nameDate}>
+						<p>Michelle Appleton</p>
+						<p>28 Jun 2020</p>
+					</div>
+					<button className={classes.shareBtn} onClick={() => setButtonIsClicked(!buttonIsClicked)}>
+						<img src="images/icon-share.svg" alt="" />
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
