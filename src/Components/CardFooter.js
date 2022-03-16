2404aa60a5859d63454dfd2b618aa42e4a50adea
Eric Lehmann <lehm_eric@yahoo.fr>
2022-03-16 18:50:09 +0100

    desktop layout + tooltip share btn

--- src/Components/CardFooter.js
+++ src/Components/CardFooter.js
@@ -5,31 +5,25 @@ const CardFooter = () => {
 	const [buttonIsClicked, setButtonIsClicked] = useState(false);
 
 	return (
-		<div>
-			{buttonIsClicked ? (
-				<div className={`${classes.cardFooter} ${classes.footerClicked}`}>
-					<p>SHARE</p>
-					<div className={classes.socialIcons}>
-						<img src="images/icon-facebook.svg" alt="" />
-						<img src="images/icon-twitter.svg" alt="" />
-						<img src="images/icon-pinterest.svg" alt="" />
-					</div>
-					<button className={classes.shareBtn} onClick={() => setButtonIsClicked(!buttonIsClicked)}>
-						<img className={classes.shareClicked} src="images/icon-share.svg" alt="" />
-					</button>
+		<div className={`${classes.cardFooter} ${buttonIsClicked ? classes.cardFooterClicked : ""}`}>
+			<div className={`${classes.socialMediaHidden} ${buttonIsClicked ? classes.socialMediaVisible : ""}`}>
+				<p>SHARE</p>
+				<div className={classes.socialIcons}>
+					<img src="images/icon-facebook.svg" alt="" />
+					<img src="images/icon-twitter.svg" alt="" />
+					<img src="images/icon-pinterest.svg" alt="" />
 				</div>
-			) : (
-				<div className={classes.cardFooter}>
-					<img className={classes.avatar} src="images/avatar-michelle.jpg" alt="" />
-					<div className={classes.nameDate}>
-						<p>Michelle Appleton</p>
-						<p>28 Jun 2020</p>
-					</div>
-					<button className={classes.shareBtn} onClick={() => setButtonIsClicked(!buttonIsClicked)}>
-						<img src="images/icon-share.svg" alt="" />
-					</button>
+			</div>
+			<div className={`${classes.footerText} ${buttonIsClicked ? classes.footerTextHidden : ""}`}>
+				<img className={classes.avatar} src="images/avatar-michelle.jpg" alt="" />
+				<div className={classes.nameDate}>
+					<p>Michelle Appleton</p>
+					<p>28 Jun 2020</p>
 				</div>
-			)}
+			</div>
+			<button className={classes.shareBtn} onClick={() => setButtonIsClicked(!buttonIsClicked)}>
+				<img src="images/icon-share.svg" alt="" />
+			</button>
 		</div>
 	);
 };
